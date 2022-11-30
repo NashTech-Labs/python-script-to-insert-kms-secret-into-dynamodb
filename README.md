d58e6bbfc3824ffefbd780d9a3dda07e61c2e328
Mohit Saxena <AIUNVV6@amway.com>
2022-11-30 15:10:53 +0530

    removed retrive secret function

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # How To Use?
 
 1. Pass in the hash and value as parameters.
-2. There is an additional function to fetch the secret from DynamoDB as well. 
--- dynamo.py
+++ dynamo.py
@@ -23,20 +23,3 @@ def insert_dynamo_secret(hash, value):
             }
         }
     )
-
-def retrieve_dynamo_secret(hash):
-    kms = boto3.client("kms")
-    dynamo = boto3.client("dynamodb")
-
-    response = dynamo.get_item(
-        TableName=os.environ["DYNAMO_NAME"],
-        Key={
-            "EntryHash": {
-                "S": hash
-            }
-        }
-    )
-
-    decrypted = kms.decrypt(KeyId=os.environ["KEY_ARN"],
-                            CiphertextBlob=base64.b64decode(response["Item"]["Value"]["S"].encode("utf-8")))
-    return decrypted["Plaintext"].decode("utf-8")
